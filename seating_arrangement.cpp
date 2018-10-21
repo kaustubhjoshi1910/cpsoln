59a43c83f4767b9cf8e8759264fa062ad776ce8d
Kaustubh Joshi <kaustubh19j@gmail.com>
2018-10-21 10:33:42 +0530

    Hackerearth seating arrangement solution

--- seating_arrangement.cpp
+++ seating_arrangement.cpp
@@ -1,3 +1,39 @@
+/*Seating Arrangement
+Attempted by: 19552/Accuracy: 52%/Maximum Score: 10/
+ 659 Votes
+Tag(s): Very-Easy
+PROBLEM
+EDITORIAL
+MY SUBMISSIONS
+ANALYTICS
+Akash and Vishal are quite fond of travelling. They mostly travel by railways. They were travelling in a train one day and they got interested in the seating arrangement of their compartment. The compartment looked something like
+
+
+So they got interested to know the seat number facing them and the seat type facing them. The seats are denoted as follows :
+
+Window Seat : WS
+Middle Seat : MS
+Aisle Seat : AS
+
+You will be given a seat number, find out the seat number facing you and the seat type, i.e. WS, MS or AS.
+
+INPUT
+First line of input will consist of a single integer T denoting number of test-cases. Each test-case consists of a single integer N denoting the seat-number.
+
+OUTPUT
+For each test case, print the facing seat-number and the seat-type, separated by a single space in a new line.
+
+CONSTRAINTS
+1<=T<=105
+1<=N<=108
+SAMPLE INPUT
+2
+18
+40
+SAMPLE OUTPUT
+19 WS
+45 AS*/
+
 #include<iostream>
 #include<bits/stdc++.h>
 #include<math.h>
